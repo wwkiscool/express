c72fe93f6a600f7300b4429777b7fe14dd4551b6
吴伟康 <1198949718@qq.com>
2019-09-19 17:48:52 +0800

    新建数据库

---
+++ utils/config.js
@@ -0,0 +1,12 @@
+var express = require('express');
+// var app = express();
+var mysql = require('mysql');
+
+let config = {
+    host: '192.168.8.100',
+    user: 'mspdev',
+    password: 'mspdev',
+    database: 'workdabse'
+}
+
+module.exports = mysql;
