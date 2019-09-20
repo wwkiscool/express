8916dc6e1a1e5410ea363e0e4ba030ce176227f4
吴伟康 <1198949718@qq.com>
2019-09-20 19:52:34 +0800

    数据库查询

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mineRouter = require('./routes/mine');
+var bodyParser = require('body-parser')
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/mine.js
+++ routes/mine.js
@@ -4,14 +4,39 @@ var db = require('../db/config')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    db.query('select * from lsattendance', [], (err, rows) => {
-        // var result = JSON.stringify(rows);
-        res.send({
-            state: '1',
-            msg:'查询成功',
-            data: rows
-        })
-    })
+	db.query('select * from lsattendance', (err, rows) => {
+		// var result = JSON.stringify(rows);
+		res.send({
+			state: '1',
+			msg: '查询成功',
+			data: rows
+		})
+	})
 })
 
+router.post('/only', (req, res, next) => {
+	var operatorid = req.query.operatorid;
+	console.log(operatorid)
+	db.query('select * from lsattendance where operator = '+ operatorid, (err, rows) => {
+			res.send({
+				state: '1',
+				msg: '条件查询',
+				data: rows
+			})
+	})
+})
+
+ router.post('/pagination', (req, res, next) =>{
+	 var eventPlace = req.query.eventPlace;
+	 var curPage = req.query.curPage;
+	 var pageSize = 10;
+	 var result = (curPage - 1)* pageSize
+	 db.query(`select * from lscalendar where eventPlace = '${eventPlace}' order by makeDate asc limit ${result} , ${pageSize}`, (err, row) => {
+			 res.send({
+				state: '1',
+				msg: '分页查询',
+				data: row
+			 })
+		 })
+		})
 module.exports = router;
