41f11fc8ed8f709f312dae614d2e2fe51bdbca55
wwk <1198949718@qq.com>
2019-11-12 21:12:52 +0800

    部署阿里云

--- db/config.js
+++ db/config.js
@@ -8,25 +8,25 @@ var mysql = require('mysql');
 // }
 
 let config = {
-    host: 'localhost',
-	user: 'root',
-	port: 3306,
-	password: 'root',
-	database: 'test1'
+  host: 'localhost',
+  user: 'root',
+  port: 3306,
+  password: 'root',
+  database: 'test'
 }
 
 
 let pool = mysql.createPool(config);
 
 function responseDoReturn(res, ret) {
-	if (typeof ret === 'undefined') {
-		res.json({
-			code: '-1',
-			msg: '操作失败'
-		})
-	} else {
-		res.json(ret);
-	}
+  if (typeof ret === 'undefined') {
+    res.json({
+      code: '-1',
+      msg: '操作失败'
+    })
+  } else {
+    res.json(ret);
+  }
 }
 
 /**
@@ -36,42 +36,42 @@ function responseDoReturn(res, ret) {
  * @param {*} callback
  */
 function query(sql, callback) {
-	pool.getConnection((err, res) => {
-		if (err) {
-			callback(err)
-		}
+  pool.getConnection((err, res) => {
+    if (err) {
+      callback(err)
+    }
 
-		res.query(sql, (err, rows) => {
-			callback(err, rows)
-			// console.log(rows)
-			res.release()
-		});
+    res.query(sql, (err, rows) => {
+      callback(err, rows)
+      // console.log(rows)
+      res.release()
+    });
 
-	})
+  })
 }
 
 /**
  * 封装query sql带占位符
  */
-function queryArgs(sql,args,callback) {
-	pool.getConnection((err,connection) => {
+function queryArgs(sql, args, callback) {
+  pool.getConnection((err, connection) => {
 
-    console.log('2',arguments)
-		if (err) {
-			callback(err)
-		} else {
-      
-			connection.query(sql,args,(err,rows) => {
-				connection.release;
-        callback(err,rows);
-				// 释放链接
-			})
-		}
-	})
+    console.log('2', arguments)
+    if (err) {
+      callback(err)
+    } else {
+
+      connection.query(sql, args, (err, rows) => {
+        connection.release;
+        callback(err, rows);
+        // 释放链接
+      })
+    }
+  })
 }
 
 module.exports = {
-	query,
-	queryArgs,
-	responseDoReturn
+  query,
+  queryArgs,
+  responseDoReturn
 }
